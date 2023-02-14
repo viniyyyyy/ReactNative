1cb9a9d4b70c5a5a5efd24eda4adb97d87b4c6db
unknown <you@example.com>
2023-02-14 10:10:22 -0300

    Primeira versão do React Native

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app!
+
+      </Text>
+      <Text>
+        Open up App.js to start working on your app!
+
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
